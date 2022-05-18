3c2d1387dbceea48c094a1b4b2f8ae3d9de10db8
khourysamir <samir.khoury1998@gmail.com>
2022-05-17 23:32:23 -0700

    home-page-done

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react'
-import { Nav, NavItem } from 'reactstrap'
+import React, { useState } from 'react';
+import { Nav, NavItem, Navbar, NavbarToggler, NavLink, NavbarBrand, NavbarText, Collapse, DropdownToggle, UncontrolledDropdown, DropdownItem, DropdownMenu } from 'reactstrap'
 
-class Header extends Component {
-  render() {
-    const {
-      logged_in,
-      current_user,
-      new_user_route,
-      sign_in_route,
-      sign_out_route
-    } = this.props
-    console.log("logged_in:", logged_in)
-    console.log("current_user:", current_user)
-    return (
-      <>
-        <h1 className='heading-text'>WELCOME TO BIGBODYAPARTMENTS!</h1>
-        <Nav>
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const {
+    logged_in,
+    current_user,
+    new_user_route,
+    sign_in_route,
+    sign_out_route
+  } = props;
+  console.log("logged_in:", logged_in)
+  console.log("current_user:", current_user)
+  return (
+    <>
+
+      {/* <Nav>
           {logged_in &&
             <NavItem className='navBar' >
               <a href={sign_out_route} className="nav-link">Sign Out</a>
@@ -31,9 +35,67 @@ class Header extends Component {
               <a href={new_user_route} className="nav-link">Sign Up</a>
             </NavItem>
           }
-        </Nav>
-      </>
-    )
-  }
+        </Nav> */}
+
+      <div>
+        <Navbar
+          color="dark"
+          expand="md"
+          dark
+        >
+          <NavbarBrand href="/">
+            BIGBODYAPARTMENTS
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse navbar isOpen={isOpen}>
+            <Nav
+              className="me-auto"
+              navbar
+            >
+              <NavItem>
+                <NavLink href="/ApartmentIndex/">
+                  VIEW APARTMENTS
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/ApartmentNew">
+                  SAVED APARTMENTS
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown
+                inNavbar
+                nav
+              >
+                <DropdownToggle
+                  caret
+                  nav
+                >
+                  MY APARTMENTS
+                </DropdownToggle>
+                <DropdownMenu end>
+                  <NavLink href="/ApartmentIndex/">
+                    ADD AN APARTMENT
+                  </NavLink>
+                  <NavLink href="/ApartmentIndex/">
+                    EDIT MY APARTMENTS
+                  </NavLink>
+                  <NavLink href="/ApartmentIndex/">
+                    VIEW MY APARTMENTS
+                  </NavLink >
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+            <NavLink href="/ApartmentIndex/">
+              SIGN IN
+            </NavLink>
+            <NavLink href="/ApartmentIndex/">
+              SIGN UP
+            </NavLink>
+          </Collapse>
+        </Navbar>
+      </div>
+
+    </>
+  )
 }
 export default Header
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,7 +3,15 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      <h3>DO YOU HAVE AN APARTMENT FOR RENT? ARE OFFERING COMPETIIVE PRICES? IS YOUR APARTMENT BEAUTIFUL? IF YOU ANSWER YES TO ANY OF THESE QUESTIONS THEN BIGBODYAPARTMENTS IS THE PLACE FOR YOU! HERE AT BIGBODYAPARTMENTS WE OFFER VARIETY OF LUXARY APARTMENTS AT AFFORDABLE PRICES!  </h3>
+
+      <section className='hero'>
+        <img src='https://androthemes.com/themes/html/nycondos/assets/img/banner/04.jpg' alt='' />
+        <div className='hero-content'>
+          <h1 className='heading-text'>WELCOME TO BIGBODYAPARTMENTS!</h1>
+
+          <h3>DO YOU HAVE AN APARTMENT FOR RENT? ARE OFFERING COMPETIIVE PRICES? IS YOUR APARTMENT BEAUTIFUL? IF YOU ANSWER YES TO ANY OF THESE QUESTIONS THEN BIGBODYAPARTMENTS IS THE PLACE FOR YOU! HERE AT BIGBODYAPARTMENTS WE OFFER VARIETY OF LUXARY APARTMENTS AT AFFORDABLE PRICES!  </h3>
+        </div>
+      </section>
     )
   }
 }
