cad6e1d713728e9ce5ed0fc8f19c13e667462c29
khourysamir <samir.khoury1998@gmail.com>
2022-05-19 20:20:00 -0700

    showdone

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+import { Button } from 'reactstrap'
 
 export default class ApartmentShow extends Component {
   render() {
+    let { apartment } = this.props
+
     return (
-      <h3>ApartmentShow</h3>
-    )
+      <>
+        {apartment &&
+          <div>
+            <p>Street: {apartment.street}</p>
+            <p>City: {apartment.city}</p>
+            <p>State: {apartment.state}</p>
+            <p>Price: {apartment.price}</p>
+            <p>Bedrooms: {apartment.bedrooms}</p>
+            <p>Bathrooms: {apartment.bathrooms}</p>
+            <p>Pets: {apartment.pets}</p>
+            <p>Manager: {apartment.manager}</p>
+            <p>Manager Email: {apartment.email}</p>
+            <img src={apartment.image} width="200px" />
+            <Button>
+              <NavLink to={`/apartmentindex`}> Go Back </NavLink>
+            </Button>
+          </div>
+
+        }
+      </>)
   }
 }
